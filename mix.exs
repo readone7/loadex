553c83bf3cff4648dba032e726ef74599b834118
readone7 <readonegoke@icloud.com>
2019-05-03 16:25:19 -0700

    added httpoison and timex to mix file

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Loadex.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
+    [ mod: {Loadex, []},
       extra_applications: [:logger]
     ]
   end
@@ -21,6 +21,9 @@ defmodule Loadex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 1.4"},
+      {:timex, "~> 3.1"},
+      {:tzdata, "~> 0.1.8"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
