b33ae03fa19b3a6a787d74b62032adf2377915e0
readone7 <readonegoke@icloud.com>
2019-05-04 23:08:40 -0700

    convenience function to run loadex

--- lib/loadex.ex
+++ lib/loadex.ex
@@ -2,17 +2,20 @@ defmodule Loadex do
   @moduledoc """
   Documentation for Loadex.
   """
-
   @doc """
   Hello world.
 
   ## Examples
 
-      iex> Loadex.hello()
-      :world
+      iex> Loadex.run()
+      {:ok, msecs}
 
   """
-  def hello do
-    :world
+  def run(n_workers, url) when n_workers > 0 do
+    worker_func = fn -> Loadex.Worker.start(url)
+    1..n_workers
+    |> Enum.map(fn _ -> Task.async(worker_func) end)
+    |> Enum.map(&Task.await(&1))
   end
+
 end
