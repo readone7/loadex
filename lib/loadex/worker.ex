24fdfb87e0c2027050804e4b19491ef85eeb9d11
readone7 <readonegoke@icloud.com>
2019-05-04 22:54:00 -0700

    implemented naive version of worker

--- lib/loadex/worker.ex
+++ lib/loadex/worker.ex
@@ -3,7 +3,23 @@ defmodule Loadex.Worker do
   require Logger
 
   def start(url) do
-    IO.puts "hello, #{url}"
+    {timestamp, response} = Duration.measure(fn -> HTTPoison.get(url) end)
+    handle_response({Duration.to_milliseconds(timestamp), response})
+  end
+
+  defp handle_response({msecs, {:ok, %HTTPoison.Response{status_code: code}}}) when code >= 200 and code <= 304 do
+    Logger.info "worker completed in #{msecs}"
+    {:ok, msecs}
+  end
+
+  defp handle_response({_msecs, {:error, reason}}) do
+    Logger.info "worker errordue to #{inspect reason}"
+    {:error, reason}
+  end
+
+  defp handle_response({_msecs, _}) do
+    Logger.info "worker errored out"
+    {:error, :unknown}
   end
 
 end
